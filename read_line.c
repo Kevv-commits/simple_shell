0a52e46a1b543e4dbc20e969ae1646b54b87c466
Kevv-commits <knganga28@gmail.com>
2021-08-20 05:21:13 -0700

    fixed betty

--- read_line.c
+++ read_line.c
@@ -2,43 +2,49 @@
 #define READ_LINE_BUFSIZE 1024
 
 /**
- * hsh_read_line-Reads lines of input from stdin
+ * read_line- reads lines of input from stdin
  *
  * @void: no parameters taken.
- * Return:
+ * Return: buffer
  */
 
-char *hsh_read_line(void)
+char *read_line(void)
 {
 	int bufsize = READ_LINE_BUFSIZE;
 	int position = 0;
 	char *buffer = malloc(sizeof(char) * bufsize);
 	int c;
 
-	if (!buffer) {
-		fprintf(stderr, "lsh: allocation error\n");
+	if (!buffer)
+	{
+		fprintf(stderr, "hsh: allocation error\n");
 		exit(EXIT_FAILURE);
 	}
 
-	while (1) {
-		// Read a character
+	while (1)
+	{
+		/* Read a character*/
 		c = getchar();
 
-		// If we hit EOF, replace it with a null character and return.
-		if (c == EOF || c == '\n') {
+		/* If we hit EOF, replace it with a null character and return.*/
+		if (c == EOF || c == '\n')
+		{
 			buffer[position] = '\0';
-			return buffer;
-		} else {
+			return (buffer);
+		} else if
+		{
 			buffer[position] = c;
 		}
 		position++;
 
-		// If we have exceeded the buffer, reallocate.
-		if (position >= bufsize) {
-			bufsize += LSH_RL_BUFSIZE;
+		/* If we have exceeded the buffer, reallocate.*/
+		if (position >= bufsize)
+		{
+			bufsize += READ_LINE_BUFSIZE;
 			buffer = realloc(buffer, bufsize);
-			if (!buffer) {
-				fprintf(stderr, "lsh: allocation error\n");
+			if (!buffer)
+			{
+				fprintf(stderr, "hsh: allocation error\n");
 				exit(EXIT_FAILURE);
 			}
 		}
