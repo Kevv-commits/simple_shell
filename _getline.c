e1538434219cbd713f4436d65a095de91a806264
olajay-pro <olorun208@gmail.com>
2021-08-20 11:47:13 -0700

    getline/environ

---
+++ _getline.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *_getline - takes string as an input from stream
+ *@lineptr: buffer which stores the input
+ *@n: size of the buffer
+ *@stream: the stream to read from
+ *Return: the number of character read
+ */
+
+ssize_t _getline(char **lineptr, size_t *n, FILE *stream)
+{
+
+}
