b6ad329d29fb43a6a2cecdd72bbf110d1eb131d2
Olorunbunmi Olajumoke <62801575+olajay-pro@users.noreply.github.com>
2021-08-24 13:47:05 +0100

    addition of functions

--- main.h
+++ main.h
@@ -13,6 +13,13 @@ char *read_line(void);
 char **parse_line(char *line);
 int execute(char **args);
 int launch_hsh(char **args);
+ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
+int hsh_env(char **args, char __attribute__((__unused__)) **start;
+char **_copyenv(void);
+void free_env(void);
+char **_getenv(const char *var);
+void _setenv(sev_t *sev);
+void _unsetenv(sev_t *sev);
 
 /*
   Function Declarations for builtin shell commands:
